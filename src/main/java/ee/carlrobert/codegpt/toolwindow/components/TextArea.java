7d3cd555f6b59430711c74473f94593fdca3960f
Michael Best <michael.best@gmail.com>
2023-03-28 17:28:04 -0600

    Remove color styling on input box text

--- src/main/java/ee/carlrobert/codegpt/toolwindow/components/TextArea.java
+++ src/main/java/ee/carlrobert/codegpt/toolwindow/components/TextArea.java
@@ -3,7 +3,6 @@ package ee.carlrobert.codegpt.toolwindow.components;
 import static ee.carlrobert.codegpt.util.SwingUtils.addShiftEnterInputMap;
 import static ee.carlrobert.codegpt.util.SwingUtils.createIconButton;
 
-import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBTextArea;
 import com.intellij.util.ui.JBUI;
 import icons.Icons;
@@ -16,7 +15,6 @@ public class TextArea extends JBTextArea {
   public TextArea(Runnable onSubmit, JScrollPane textAreaScrollPane) {
     super();
     getEmptyText().setText("Ask me anything...");
-    setForeground(JBColor.GRAY);
     setMargin(JBUI.insets(5));
     addSubmitButton(onSubmit, textAreaScrollPane);
     addShiftEnterInputMap(this, onSubmit);
