2a8879f45fb650b9fc52d1977433fa6232f3c3ed
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2023-10-02 00:50:36 +0300

    Remove unnecessary code

--- src/main/java/ee/carlrobert/codegpt/toolwindow/chat/ChatToolWindowTabPanelEditor.java
+++ src/main/java/ee/carlrobert/codegpt/toolwindow/chat/ChatToolWindowTabPanelEditor.java
@@ -7,9 +7,9 @@ import static java.lang.String.format;
 import com.intellij.diff.DiffContentFactory;
 import com.intellij.diff.DiffDialogHints;
 import com.intellij.diff.DiffManager;
-import com.intellij.diff.contents.DiffContent;
 import com.intellij.diff.requests.SimpleDiffRequest;
 import com.intellij.diff.util.DiffUserDataKeys;
+import com.intellij.diff.util.DiffUtil;
 import com.intellij.diff.util.Side;
 import com.intellij.icons.AllIcons.Actions;
 import com.intellij.ide.util.EditorHelper;
@@ -19,15 +19,10 @@ import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.editor.EditorKind;
-import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.ex.EditorEx;
 import com.intellij.openapi.editor.impl.ContextMenuPopupHandler;
@@ -41,7 +36,6 @@ import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.testFramework.LightVirtualFile;
 import com.intellij.ui.JBColor;
@@ -51,7 +45,6 @@ import com.intellij.util.ui.FormBuilder;
 import com.intellij.util.ui.JBUI;
 import ee.carlrobert.codegpt.actions.toolwindow.ReplaceCodeInMainEditorAction;
 import ee.carlrobert.codegpt.toolwindow.IconActionButton;
-import ee.carlrobert.codegpt.util.DiffUtils;
 import ee.carlrobert.codegpt.util.EditorUtils;
 import ee.carlrobert.codegpt.util.FileUtils;
 import ee.carlrobert.codegpt.util.OverlayUtils;
@@ -61,10 +54,6 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.MouseEvent;
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
@@ -78,8 +67,6 @@ public class ChatToolWindowTabPanelEditor implements Disposable {
   private final Project project;
   private final Editor editor;
   private final Map.Entry<String, String> fileNameExtensionMapping;
-
-  private final DiffManager diffManagerInstance = DiffManager.getInstance();
 
   public ChatToolWindowTabPanelEditor(
       Project project,
@@ -170,6 +157,17 @@ public class ChatToolWindowTabPanelEditor implements Disposable {
     return wrapper;
   }
 
+  private void displayWarning(AnActionEvent event) {
+    var locationOnScreen = ((MouseEvent) event.getInputEvent()).getLocationOnScreen();
+    locationOnScreen.y = locationOnScreen.y - 16;
+
+    OverlayUtils.showWarningBalloon(
+        EditorUtils.getSelectedEditor(project) == null
+            ? "Unable to locate a selected editor"
+            : "Please select a target code before proceeding",
+        locationOnScreen);
+  }
+
   class DiffAction extends AnAction {
 
     DiffAction() {
@@ -178,23 +176,25 @@ public class ChatToolWindowTabPanelEditor implements Disposable {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
-      String text = editor.getDocument().getText();
-      Application application = ApplicationManager.getApplication();
-      var locationOnScreen = ((MouseEvent) event.getInputEvent()).getLocationOnScreen();
-      locationOnScreen.y = locationOnScreen.y - 16;
+      var selectedTextEditor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+      if (!EditorUtils.hasSelection(selectedTextEditor)) {
+        displayWarning(event);
+        return;
+      }
 
-      application.invokeLater(() -> application.runWriteAction(
-          () -> WriteCommandAction.runWriteCommandAction(event.getProject(), () -> {
-            try {
-              assert event.getProject() != null;
-              Editor editor = FileEditorManager.getInstance(event.getProject())
-                  .getSelectedTextEditor();
-              openDiffView(event.getProject(), editor, text);
-              OverlayUtils.showInfoBalloon("Diff opened!", locationOnScreen);
-            } catch (Exception ignore) {
-              OverlayUtils.showInfoBalloon("Diff failed!", locationOnScreen);
-            }
-          })));
+      var resultEditorFile = FileUtils.getEditorFile(selectedTextEditor);
+      var diffContentFactory = DiffContentFactory.getInstance();
+      var request = new SimpleDiffRequest(
+          "CodeGPT Diff",
+          diffContentFactory.create(project, FileUtils.getEditorFile(editor)),
+          diffContentFactory.create(project, resultEditorFile),
+          "CodeGPT suggested code",
+          resultEditorFile.getName());
+      request.putUserData(
+          DiffUserDataKeys.SCROLL_TO_LINE,
+          Pair.create(Side.RIGHT, DiffUtil.getCaretPosition(selectedTextEditor).line));
+
+      DiffManager.getInstance().showDiff(project, request, DiffDialogHints.DEFAULT);
     }
   }
 
@@ -305,66 +305,9 @@ public class ChatToolWindowTabPanelEditor implements Disposable {
         if (EditorUtils.isMainEditorTextSelected(project)) {
           EditorUtils.replaceMainEditorSelection(project, editor.getDocument().getText());
         } else {
-          var locationOnScreen = ((MouseEvent) event.getInputEvent()).getLocationOnScreen();
-          locationOnScreen.y = locationOnScreen.y - 16;
-
-          OverlayUtils.showWarningBalloon(
-              EditorUtils.getSelectedEditor(project) == null
-                  ? "Unable to locate a selected editor"
-                  : "Please select a target code before proceeding",
-              locationOnScreen);
+          displayWarning(event);
         }
       }
-    }
-  }
-
-  public void openDiffView(@NotNull Project project, Editor editor, String text) {
-    try {
-      SelectionModel selectionModel = editor.getSelectionModel();
-      Document document = editor.getDocument();
-      int startLine = document.getLineNumber(selectionModel.getSelectionStart());
-      int endLine = document.getLineNumber(selectionModel.getSelectionEnd());
-      VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
-      assert virtualFile != null;
-      String fileName = virtualFile.getName();
-      String filePath = virtualFile.getPath();
-      int startLineOffset = document.getLineStartOffset(startLine);
-      int endLineOffset = document.getLineEndOffset(endLine);
-      final String selectedText = document.getText().substring(startLineOffset, endLineOffset);
-      String newText = FileUtils.addWriteSpace(text, selectedText);
-      VirtualFile rightFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(filePath);
-      Path path = Paths.get(PathManager.getTempPath() + File.separator + DiffUtils.TEMP_DIR_NAME);
-      Files.createDirectories(path);
-      var tempFileName =
-          FileUtils.getName(fileName, "") + "_" + DateTimeFormatter.ofPattern("yyyyMMddHHmmss")
-              .format(LocalDateTime.now()) + "." + virtualFile.getExtension();
-      String tempFilePath =
-          PathManager.getTempPath() + File.separator + DiffUtils.TEMP_DIR_NAME + File.separator
-              + tempFileName;
-      FileUtils.replaceContentInFile(filePath, tempFilePath, selectedText, newText, startLine,
-          endLine);
-      File leftFileTemp = new File(tempFilePath);
-      VirtualFile leftFile = LocalFileSystem.getInstance()
-          .refreshAndFindFileByPath(leftFileTemp.getCanonicalPath());
-      assert leftFile != null;
-      DiffContent leftContent = DiffContentFactory.getInstance().create(project, leftFile);
-      assert rightFile != null;
-      DiffContent rightContent = DiffContentFactory.getInstance().create(project, rightFile);
-      String leftFileTitle = leftFile.getName() + DiffUtils.LEFT_TITLE;
-      String rightFileTitle = rightFile.getName() + DiffUtils.RIGHT_TITLE;
-      SimpleDiffRequest simpleDiffRequest = new SimpleDiffRequest(DiffUtils.DIFF_TITLE, leftContent,
-          rightContent, leftFileTitle, rightFileTitle);
-      simpleDiffRequest.putUserData(DiffUserDataKeys.SCROLL_TO_LINE,
-          Pair.create(Side.RIGHT, startLine));
-      simpleDiffRequest.putUserData(DiffUserDataKeys.PREFERRED_FOCUS_SIDE, Side.RIGHT);
-      simpleDiffRequest.putUserData(DiffUtils.DIFF_FILENAME, fileName);
-      simpleDiffRequest.putUserData(DiffUtils.DIFF_FILEPATH_LEFT, leftFile.getPath());
-      simpleDiffRequest.putUserData(DiffUtils.DIFF_FILEPATH_RIGHT, rightFile.getPath());
-      simpleDiffRequest.putUserData(DiffUtils.DIFF_FILE_UNIQUE_ID, DiffUtils.DIFF_ID);
-      DiffUtils.closeDiffViewIfAlreadyOpened(project);
-      diffManagerInstance.showDiff(project, simpleDiffRequest, DiffDialogHints.DEFAULT);
-    } catch (Exception e) {
-      throw new RuntimeException("Open diff failed");
     }
   }
 }
