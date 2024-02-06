fe4e02f7f6abdb1ec056df4fcb956ab499262c8e
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-02-06 02:18:53 +0200

    Revert "Revert "feat: code completion improvements""
    This reverts commit 7f586da0c185253b8e399ec118ca441c8cfebf3d.

--- src/main/java/ee/carlrobert/codegpt/actions/EnableCompletionsAction.java
+++ src/main/java/ee/carlrobert/codegpt/actions/EnableCompletionsAction.java
@@ -1,15 +1,15 @@
 package ee.carlrobert.codegpt.actions;
 
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.DumbAwareAction;
 import ee.carlrobert.codegpt.settings.configuration.ConfigurationState;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Enables code-completion.<br/> Publishes message to {@link CodeCompletionEnabledListener#TOPIC}
  */
-public class EnableCompletionsAction extends AnAction {
+public class EnableCompletionsAction extends DumbAwareAction {
 
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
