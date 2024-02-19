5a88a7d9f3556302a222706a6f43343f14a89aaf
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-02-19 14:33:25 +0200

    feat: hide code completion feature for Azure and You service

--- src/main/java/ee/carlrobert/codegpt/actions/DisableCompletionsAction.java
+++ src/main/java/ee/carlrobert/codegpt/actions/DisableCompletionsAction.java
@@ -1,10 +1,15 @@
 package ee.carlrobert.codegpt.actions;
 
+import static ee.carlrobert.codegpt.settings.service.ServiceType.LLAMA_CPP;
+import static ee.carlrobert.codegpt.settings.service.ServiceType.OPENAI;
+
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import ee.carlrobert.codegpt.codecompletions.CodeGPTEditorManager;
+import ee.carlrobert.codegpt.settings.GeneralSettings;
 import ee.carlrobert.codegpt.settings.configuration.ConfigurationSettings;
+import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -23,8 +28,10 @@ public class DisableCompletionsAction extends AnAction {
 
   @Override
   public void update(@NotNull AnActionEvent e) {
+    var selectedService = GeneralSettings.getCurrentState().getSelectedService();
     var codeCompletionEnabled = ConfigurationSettings.getCurrentState().isCodeCompletionsEnabled();
     e.getPresentation().setEnabled(codeCompletionEnabled);
-    e.getPresentation().setVisible(codeCompletionEnabled);
+    e.getPresentation()
+        .setVisible(codeCompletionEnabled && List.of(OPENAI, LLAMA_CPP).contains(selectedService));
   }
 }
