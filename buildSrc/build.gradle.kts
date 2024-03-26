9b35f6a69f579ab1f628f99c42101c0f350b6cd1
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-03-26 17:27:30 +0200

    add necessary kotlin deps

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,4 +9,5 @@ repositories {
 
 dependencies {
   implementation("org.jetbrains.intellij.plugins", "gradle-intellij-plugin", "1.17.0")
+  implementation("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.9.22")
 }
--- buildSrc/src/main/kotlin/codegpt.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/codegpt.java-conventions.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
@@ -5,6 +7,7 @@ plugins {
   id("java")
   id("idea")
   id("org.jetbrains.intellij")
+  id("org.jetbrains.kotlin.jvm")
 }
 
 version = properties("pluginVersion")
@@ -37,6 +40,9 @@ tasks {
     withType<JavaCompile> {
       sourceCompatibility = it
       targetCompatibility = it
+    }
+    withType<KotlinCompile> {
+      kotlinOptions.jvmTarget = it
     }
   }
 
