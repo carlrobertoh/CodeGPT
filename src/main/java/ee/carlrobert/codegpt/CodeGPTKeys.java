a87c9e3b0ac0019f5af0717cecf56a4d4e38eecb
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-03-26 22:51:45 +0200

    remove previous implementation

--- src/main/java/ee/carlrobert/codegpt/CodeGPTKeys.java
+++ src/main/java/ee/carlrobert/codegpt/CodeGPTKeys.java
@@ -1,7 +1,5 @@
 package ee.carlrobert.codegpt;
 
-import com.intellij.openapi.editor.EditorCustomElementRenderer;
-import com.intellij.openapi.editor.Inlay;
 import com.intellij.openapi.util.Key;
 import java.util.List;
 
@@ -9,10 +7,6 @@ public class CodeGPTKeys {
 
   public static final Key<String> PREVIOUS_INLAY_TEXT =
       Key.create("codegpt.editor.inlay.prev-value");
-  public static final Key<Inlay<EditorCustomElementRenderer>> SINGLE_LINE_INLAY =
-      Key.create("codegpt.editor.inlay.single-line");
-  public static final Key<Inlay<EditorCustomElementRenderer>> MULTI_LINE_INLAY =
-      Key.create("codegpt.editor.inlay.multi-line");
   public static final Key<List<ReferencedFile>> SELECTED_FILES =
       Key.create("codegpt.selectedFiles");
 }
--- src/main/java/ee/carlrobert/codegpt/actions/DisableCompletionsAction.java
+++ src/main/java/ee/carlrobert/codegpt/actions/DisableCompletionsAction.java
@@ -7,7 +7,6 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
-import ee.carlrobert.codegpt.codecompletions.CodeGPTEditorManager;
 import ee.carlrobert.codegpt.settings.GeneralSettings;
 import ee.carlrobert.codegpt.settings.configuration.ConfigurationSettings;
 import java.util.List;
@@ -21,7 +20,6 @@ public class DisableCompletionsAction extends AnAction {
   @Override
   public void actionPerformed(@NotNull AnActionEvent e) {
     ConfigurationSettings.getCurrentState().setCodeCompletionsEnabled(false);
-    CodeGPTEditorManager.getInstance().disposeAllInlays(e.getProject());
     ApplicationManager.getApplication()
         .getMessageBus().syncPublisher(CodeCompletionEnabledListener.TOPIC)
         .onCodeCompletionsEnabledChange(false);
