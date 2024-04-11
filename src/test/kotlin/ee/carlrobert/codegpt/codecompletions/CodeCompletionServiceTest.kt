0cdd5096baf46d36a340d96e3bf37eb155f3596c
Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>
2024-04-11 11:03:31 +0200

    chore: Convert Java tests to Kotlin (#447)

---
+++ src/test/kotlin/ee/carlrobert/codegpt/codecompletions/CodeCompletionServiceTest.kt
@@ -0,0 +1,50 @@
+package ee.carlrobert.codegpt.codecompletions
+
+import com.intellij.openapi.editor.VisualPosition
+import ee.carlrobert.codegpt.CodeGPTKeys.PREVIOUS_INLAY_TEXT
+import ee.carlrobert.codegpt.settings.service.llama.LlamaSettings
+import ee.carlrobert.codegpt.util.file.FileUtil.getResourceContent
+import ee.carlrobert.llm.client.http.RequestEntity
+import ee.carlrobert.llm.client.http.exchange.StreamHttpExchange
+import ee.carlrobert.llm.client.util.JSONUtil.e
+import ee.carlrobert.llm.client.util.JSONUtil.jsonMapResponse
+import org.assertj.core.api.Assertions.assertThat
+import testsupport.IntegrationTest
+
+class CodeCompletionServiceTest : IntegrationTest() {
+
+  private val cursorPosition = VisualPosition(3, 0)
+
+  fun testFetchCodeCompletionLlama() {
+    useLlamaService()
+    LlamaSettings.getCurrentState().isCodeCompletionsEnabled = true
+    myFixture.configureByText(
+      "CompletionTest.java",
+      getResourceContent("/codecompletions/code-completion-file.txt")
+    )
+    myFixture.editor.caretModel.moveToVisualPosition(cursorPosition)
+    val expectedCompletion = "TEST_OUTPUT"
+    val prefix = """
+         ${"z".repeat(245)}
+         [INPUT]
+         c
+         """.trimIndent() // 128 tokens
+    val suffix = """
+         
+         [\INPUT]
+         ${"z".repeat(247)}
+         """.trimIndent() // 128 tokens
+    expectLlama(StreamHttpExchange { request: RequestEntity ->
+      assertThat(request.uri.path).isEqualTo("/completion")
+      assertThat(request.method).isEqualTo("POST")
+      assertThat(request.body)
+        .extracting("prompt")
+        .isEqualTo(InfillPromptTemplate.LLAMA.buildPrompt(prefix, suffix))
+      listOf(jsonMapResponse(e("content", expectedCompletion), e("stop", true)))
+    })
+
+    myFixture.type('c')
+
+    waitExpecting { "TEST_OUTPUT" == PREVIOUS_INLAY_TEXT[myFixture.editor] }
+  }
+}
---
+++ src/test/kotlin/ee/carlrobert/codegpt/completions/DefaultCompletionRequestHandlerTest.kt
@@ -0,0 +1,179 @@
+package ee.carlrobert.codegpt.completions
+
+import ee.carlrobert.codegpt.CodeGPTPlugin
+import ee.carlrobert.codegpt.completions.CompletionRequestProvider.COMPLETION_SYSTEM_PROMPT
+import ee.carlrobert.codegpt.completions.llama.PromptTemplate.LLAMA
+import ee.carlrobert.codegpt.conversations.ConversationService
+import ee.carlrobert.codegpt.conversations.message.Message
+import ee.carlrobert.codegpt.settings.configuration.ConfigurationSettings
+import ee.carlrobert.llm.client.http.RequestEntity
+import ee.carlrobert.llm.client.http.exchange.StreamHttpExchange
+import ee.carlrobert.llm.client.util.JSONUtil.e
+import ee.carlrobert.llm.client.util.JSONUtil.jsonArray
+import ee.carlrobert.llm.client.util.JSONUtil.jsonMap
+import ee.carlrobert.llm.client.util.JSONUtil.jsonMapResponse
+import org.apache.http.HttpHeaders
+import org.assertj.core.api.Assertions.assertThat
+import testsupport.IntegrationTest
+
+class DefaultCompletionRequestHandlerTest : IntegrationTest() {
+
+  fun testOpenAIChatCompletionCall() {
+    useOpenAIService()
+    val message = Message("TEST_PROMPT")
+    val conversation = ConversationService.getInstance().startConversation()
+    val requestHandler = CompletionRequestHandler(getRequestEventListener(message))
+    expectOpenAI(StreamHttpExchange { request: RequestEntity ->
+      assertThat(request.uri.path).isEqualTo("/v1/chat/completions")
+      assertThat(request.method).isEqualTo("POST")
+      assertThat(request.headers[HttpHeaders.AUTHORIZATION]!![0]).isEqualTo("Bearer TEST_API_KEY")
+      assertThat(request.body)
+        .extracting(
+          "model",
+          "messages")
+        .containsExactly(
+          "gpt-4",
+          listOf(
+            mapOf("role" to "system", "content" to COMPLETION_SYSTEM_PROMPT),
+            mapOf("role" to "user", "content" to "TEST_PROMPT")))
+      listOf(
+        jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("role", "assistant")))),
+        jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("content", "Hel")))),
+        jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("content", "lo")))),
+        jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("content", "!")))))
+    })
+
+    requestHandler.call(CallParameters(conversation, ConversationType.DEFAULT, message, false))
+
+    waitExpecting { "Hello!" == message.response }
+  }
+
+  fun testAzureChatCompletionCall() {
+    useAzureService()
+    val conversationService = ConversationService.getInstance()
+    val prevMessage = Message("TEST_PREV_PROMPT")
+    prevMessage.response = "TEST_PREV_RESPONSE"
+    val conversation = conversationService.startConversation()
+    conversation.addMessage(prevMessage)
+    conversationService.saveConversation(conversation)
+    expectAzure(StreamHttpExchange { request: RequestEntity ->
+      assertThat(request.uri.path).isEqualTo(
+        "/openai/deployments/TEST_DEPLOYMENT_ID/chat/completions")
+      assertThat(request.uri.query).isEqualTo("api-version=TEST_API_VERSION")
+      assertThat(request.headers["Api-key"]!![0]).isEqualTo("TEST_API_KEY")
+      assertThat(request.headers["X-llm-application-tag"]!![0]).isEqualTo("codegpt")
+      assertThat(request.body)
+        .extracting("messages")
+        .isEqualTo(
+          listOf(
+            mapOf("role" to "system", "content" to COMPLETION_SYSTEM_PROMPT),
+            mapOf("role" to "user", "content" to "TEST_PREV_PROMPT"),
+            mapOf("role" to "assistant", "content" to "TEST_PREV_RESPONSE"),
+            mapOf("role" to "user", "content" to "TEST_PROMPT")))
+      listOf(
+        jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("role", "assistant")))),
+        jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("content", "Hel")))),
+        jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("content", "lo")))),
+        jsonMapResponse("choices", jsonArray(jsonMap("delta", jsonMap("content", "!")))))
+    })
+    val message = Message("TEST_PROMPT")
+    val requestHandler = CompletionRequestHandler(getRequestEventListener(message))
+
+    requestHandler.call(CallParameters(conversation, ConversationType.DEFAULT, message, false))
+
+    waitExpecting { "Hello!" == message.response }
+  }
+
+  fun testYouChatCompletionCall() {
+    useYouService()
+    val message = Message("TEST_PROMPT")
+    val conversation = ConversationService.getInstance().startConversation()
+    conversation.addMessage(Message("Ping", "Pong"))
+    val requestHandler = CompletionRequestHandler(getRequestEventListener(message))
+    expectYou(StreamHttpExchange { request: RequestEntity ->
+      assertThat(request.uri.path).isEqualTo("/api/streamingSearch")
+      assertThat(request.method).isEqualTo("GET")
+      assertThat(request.uri.path).isEqualTo("/api/streamingSearch")
+      assertThat(request.uri.query).isEqualTo(
+        "q=TEST_PROMPT&"
+                + "page=1&"
+                + "cfr=CodeGPT&"
+                + "count=10&"
+                + "safeSearch=WebPages,Translations,TimeZone,Computation,RelatedSearches&"
+                + "domain=youchat&"
+                + "selectedChatMode=default&"
+                + "chat=[{\"question\":\"Ping\",\"answer\":\"Pong\"}]&"
+                + "utm_source=ide&"
+                + "utm_medium=jetbrains&"
+                + "utm_campaign=" + CodeGPTPlugin.getVersion() + "&"
+                + "utm_content=CodeGPT")
+      assertThat(request.headers)
+        .flatExtracting("Accept", "Connection", "User-agent", "Cookie")
+        .containsExactly(
+          "text/event-stream",
+          "Keep-Alive",
+          "youide CodeGPT",
+          "safesearch_guest=Moderate; "
+                  + "youpro_subscription=true; "
+                  + "you_subscription=free; "
+                  + "stytch_session=; "
+                  + "ydc_stytch_session=; "
+                  + "stytch_session_jwt=; "
+                  + "ydc_stytch_session_jwt=; "
+                  + "eg4=false; "
+                  + "__cf_bm=aN2b3pQMH8XADeMB7bg9s1bJ_bfXBcCHophfOGRg6g0-1693601599-0-"
+                  + "AWIt5Mr4Y3xQI4mIJ1lSf4+vijWKDobrty8OopDeBxY+NABe0MRFidF3dCUoWjRt8"
+                  + "SVMvBZPI3zkOgcRs7Mz3yazd7f7c58HwW5Xg9jdBjNg;")
+      listOf(
+        jsonMapResponse("youChatToken", "Hel"),
+        jsonMapResponse("youChatToken", "lo"),
+        jsonMapResponse("youChatToken", "!"))
+    })
+
+    requestHandler.call(CallParameters(conversation, ConversationType.DEFAULT, message, false))
+
+    waitExpecting { "Hello!" == message.response }
+  }
+
+  fun testLlamaChatCompletionCall() {
+    useLlamaService()
+    ConfigurationSettings.getCurrentState().maxTokens = 99
+    val message = Message("TEST_PROMPT")
+    val conversation = ConversationService.getInstance().startConversation()
+    conversation.addMessage(Message("Ping", "Pong"))
+    val requestHandler = CompletionRequestHandler(getRequestEventListener(message))
+    expectLlama(StreamHttpExchange { request: RequestEntity ->
+      assertThat(request.uri.path).isEqualTo("/completion")
+      assertThat(request.body)
+        .extracting(
+          "prompt",
+          "n_predict",
+          "stream")
+        .containsExactly(
+          LLAMA.buildPrompt(
+            COMPLETION_SYSTEM_PROMPT,
+            "TEST_PROMPT",
+            conversation.messages),
+          99,
+          true)
+      listOf<String?>(
+        jsonMapResponse("content", "Hel"),
+        jsonMapResponse("content", "lo!"),
+        jsonMapResponse(
+          e("content", ""),
+          e("stop", true)))
+    })
+
+    requestHandler.call(CallParameters(conversation, ConversationType.DEFAULT, message, false))
+
+    waitExpecting { "Hello!" == message.response }
+  }
+
+  private fun getRequestEventListener(message: Message): CompletionResponseEventListener {
+    return object : CompletionResponseEventListener {
+      override fun handleCompleted(fullMessage: String, callParameters: CallParameters) {
+        message.response = fullMessage
+      }
+    }
+  }
+}
