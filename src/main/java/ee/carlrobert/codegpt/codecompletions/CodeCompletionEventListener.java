b6be76f40c7247e01fd6e8df9896aebc55a16155
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-04-12 17:40:21 +0300

    fix: temporarily disable tree-sitter logic (fixes #452)

--- src/main/java/ee/carlrobert/codegpt/codecompletions/CodeCompletionEventListener.java
+++ src/main/java/ee/carlrobert/codegpt/codecompletions/CodeCompletionEventListener.java
@@ -7,13 +7,10 @@ import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.impl.EditorImpl;
 import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator;
 import ee.carlrobert.codegpt.CodeGPTBundle;
 import ee.carlrobert.codegpt.actions.OpenSettingsAction;
-import ee.carlrobert.codegpt.treesitter.CodeCompletionParserFactory;
 import ee.carlrobert.codegpt.ui.OverlayUtil;
-import ee.carlrobert.codegpt.util.file.FileUtil;
 import ee.carlrobert.llm.client.openai.completion.ErrorDetails;
 import ee.carlrobert.llm.completion.CompletionEventListener;
 import java.io.IOException;
@@ -49,15 +46,15 @@ class CodeCompletionEventListener implements CompletionEventListener<String> {
     try {
       var project = editor.getProject();
       if (project != null) {
-        var fileExtension =
-            FileUtil.getFileExtension(((EditorImpl) editor).getVirtualFile().getName());
-        var processedOutput = CodeCompletionParserFactory
+        /*var processedOutput = CodeCompletionParserFactory
             .getParserForFileExtension(fileExtension)
             .parse(
                 requestDetails.getPrefix(),
                 requestDetails.getSuffix(),
-                messageBuilder.toString());
-        handleComplete(processedOutput);
+                messageBuilder.toString());*/
+        var response = messageBuilder.toString();
+        handleComplete(
+            response.contains("\n") ? response.substring(0, response.indexOf("\n")) : response);
       }
     } catch (IllegalArgumentException e) {
       handleComplete(messageBuilder.toString());
