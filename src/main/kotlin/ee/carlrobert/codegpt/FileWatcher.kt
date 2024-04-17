92d9d5ee20bfc66e69635d44e3b3db326e7ec48e
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-04-17 16:33:04 +0300

    fix: file watcher disposable by making it project-level service

--- src/main/kotlin/ee/carlrobert/codegpt/FileWatcher.kt
+++ src/main/kotlin/ee/carlrobert/codegpt/FileWatcher.kt
@@ -1,29 +1,29 @@
 package ee.carlrobert.codegpt
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.components.Service
 import org.apache.commons.io.monitor.FileAlterationListenerAdaptor
 import org.apache.commons.io.monitor.FileAlterationMonitor
 import org.apache.commons.io.monitor.FileAlterationObserver
 import java.io.File
-import java.nio.file.Path
 
-class FileWatcher(private val pathToWatch: Path) : Disposable {
+@Service(Service.Level.PROJECT)
+class FileWatcher : Disposable {
 
-    private val fileMonitor =
-        FileAlterationMonitor(500, FileAlterationObserver(pathToWatch.toFile()))
+    private var fileMonitor: FileAlterationMonitor? = null
 
-    fun watch(onFileCreated: (File) -> Unit) {
-        val observer = FileAlterationObserver(pathToWatch.toFile())
+    fun watch(pathToWatch: File, onFileCreated: (File) -> Unit) {
+        val observer = FileAlterationObserver(pathToWatch)
         observer.addListener(object : FileAlterationListenerAdaptor() {
             override fun onFileCreate(file: File) {
                 onFileCreated(file)
             }
         })
-        fileMonitor.addObserver(observer)
-        fileMonitor.start()
+        fileMonitor = FileAlterationMonitor(500, observer)
+        fileMonitor?.start()
     }
 
     override fun dispose() {
-        fileMonitor.stop()
+        fileMonitor?.stop()
     }
 }
