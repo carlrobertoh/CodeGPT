940d2c75979f8cff07b265b7df863efce29eca99
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-04-28 14:27:14 +1200

    Initial implementation of model combo box

--- src/main/kotlin/ee/carlrobert/codegpt/settings/service/ollama/OllamaSettingsForm.kt
+++ src/main/kotlin/ee/carlrobert/codegpt/settings/service/ollama/OllamaSettingsForm.kt
@@ -1,27 +1,46 @@
 package ee.carlrobert.codegpt.settings.service.ollama
 
-import com.intellij.openapi.observable.util.whenTextChangedFromUi
+import com.intellij.openapi.ui.ComboBox
 import com.intellij.ui.TitledSeparator
 import com.intellij.ui.components.JBTextField
 import com.intellij.util.ui.FormBuilder
 import ee.carlrobert.codegpt.CodeGPTBundle
 import ee.carlrobert.codegpt.settings.service.CodeCompletionConfigurationForm
 import ee.carlrobert.codegpt.ui.UIUtil
+import ee.carlrobert.llm.client.ollama.OllamaClient
+import javax.swing.DefaultComboBoxModel
 
 class OllamaSettingsForm(settings: OllamaSettingsState) {
 
+    private val loadingModelsComboBoxModel = DefaultComboBoxModel(arrayOf("Loading"))
+    private val modelComboBox: ComboBox<String> = ComboBox(loadingModelsComboBoxModel).apply {
+        isEnabled = false
+    }
+
     private val hostField: JBTextField = JBTextField()
-    private val modelField: JBTextField = JBTextField()
     private val codeCompletionConfigurationForm: CodeCompletionConfigurationForm =
         CodeCompletionConfigurationForm(
             settings.isCodeCompletionEnabled(),
             settings.codeCompletionMaxTokens
         )
 
-    init {
-        modelField.whenTextChangedFromUi {
-            codeCompletionConfigurationForm.setComponentsEnabled(getCurrentState().fimTemplate != null)
-        }
+    fun refreshModels() {
+        Thread {
+            modelComboBox.apply {
+                model = loadingModelsComboBoxModel
+                isEnabled = false
+
+                val models = OllamaClient.Builder()
+                    .setHost(hostField.text)
+                    .build()
+                    .modelTags
+                    .models
+                    .map { it.name }
+
+                model = DefaultComboBoxModel(models.toTypedArray())
+                isEnabled = true
+            }
+        }.start()
     }
 
     fun getForm() = FormBuilder.createFormBuilder()
@@ -35,7 +54,7 @@ class OllamaSettingsForm(settings: OllamaSettingsState) {
                 )
                 .addLabeledComponent(
                     CodeGPTBundle.get("settingsConfigurable.shared.model.label"),
-                    modelField
+                    modelComboBox
                 )
                 .panel
         )
@@ -43,18 +62,10 @@ class OllamaSettingsForm(settings: OllamaSettingsState) {
         .addComponent(UIUtil.withEmptyLeftBorder(codeCompletionConfigurationForm.getForm()))
         .panel
 
-    fun getHost(): String {
-        return hostField.text
-    }
-
-    fun getModel(): String {
-        return modelField.text
-    }
-
     fun getCurrentState(): OllamaSettingsState {
         return OllamaSettingsState(
-            host = getHost(),
-            model = getModel(),
+            host = hostField.text,
+            model = modelComboBox.item,
             codeCompletionMaxTokens = codeCompletionConfigurationForm.maxTokens,
             codeCompletionsEnabled = codeCompletionConfigurationForm.isCodeCompletionsEnabled
         )
@@ -63,9 +74,10 @@ class OllamaSettingsForm(settings: OllamaSettingsState) {
     fun resetForm() {
         val state = OllamaSettings.getCurrentState()
         hostField.text = state.host
-        modelField.text = state.model
+        modelComboBox.item = state.model
         codeCompletionConfigurationForm.isCodeCompletionsEnabled = state.isCodeCompletionEnabled()
         codeCompletionConfigurationForm.maxTokens = state.codeCompletionMaxTokens
-        codeCompletionConfigurationForm.setComponentsEnabled(state.fimTemplate != null)
+
+        refreshModels()
     }
 }
