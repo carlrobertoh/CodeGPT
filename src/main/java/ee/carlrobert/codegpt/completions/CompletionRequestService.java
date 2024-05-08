dcd0a3fc512a9dc2b7d005c9743d4bd6654562a3
Phil <phil.kessler@gmx.de>
2024-05-08 15:06:14 +0200

    Revert "fix: use /infill for llama.cpp code-completions (#513)" (#533)
    This reverts commit 8de72b330178fa97b0482e1dbb2829964f8b737a.

--- src/main/java/ee/carlrobert/codegpt/completions/CompletionRequestService.java
+++ src/main/java/ee/carlrobert/codegpt/completions/CompletionRequestService.java
@@ -126,7 +126,7 @@ public final class CompletionRequestService {
           CodeCompletionRequestFactory.buildCustomRequest(requestDetails),
           new OpenAITextCompletionEventSourceListener(eventListener));
       case LLAMA_CPP -> CompletionClientProvider.getLlamaClient()
-          .getInfillAsync(
+          .getChatCompletionAsync(
               CodeCompletionRequestFactory.buildLlamaRequest(requestDetails),
               eventListener);
       case OLLAMA -> CompletionClientProvider.getOllamaClient().getCompletionAsync(
