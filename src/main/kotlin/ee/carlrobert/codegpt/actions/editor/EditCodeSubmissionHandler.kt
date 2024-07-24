45c0a490783c8b70f851aca38848c8a90e528744
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-07-24 20:03:40 +0300

    fix: wrong line separators on code completion (fixes #637)

--- src/main/kotlin/ee/carlrobert/codegpt/actions/editor/EditCodeSubmissionHandler.kt
+++ src/main/kotlin/ee/carlrobert/codegpt/actions/editor/EditCodeSubmissionHandler.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.command.WriteCommandAction.runWriteCommandAction
 import com.intellij.openapi.components.service
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.util.text.StringUtil
 import com.jetbrains.rd.util.AtomicReference
 import ee.carlrobert.codegpt.completions.CompletionClientProvider
 import ee.carlrobert.codegpt.completions.CompletionRequestProvider
@@ -58,7 +59,11 @@ class EditCodeSubmissionHandler(
 
     private fun revertAllChanges(prevSource: String) {
         runWriteCommandAction(editor.project) {
-            editor.document.replaceString(0, editor.document.textLength, prevSource)
+            editor.document.replaceString(
+                0,
+                editor.document.textLength,
+                StringUtil.convertLineSeparators(prevSource)
+            )
         }
     }
 }
