94a273ce9e822fa7a34af8abef38ea6eb82113c8
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-08-19 01:17:52 +0300

    feat: image support for Custom OpenAI provider (#649)

--- src/main/java/ee/carlrobert/codegpt/settings/service/ServiceType.java
+++ src/main/java/ee/carlrobert/codegpt/settings/service/ServiceType.java
@@ -1,15 +1,7 @@
 package ee.carlrobert.codegpt.settings.service;
 
-import static ee.carlrobert.llm.client.openai.completion.OpenAIChatCompletionModel.GPT_4_O;
-import static ee.carlrobert.llm.client.openai.completion.OpenAIChatCompletionModel.GPT_4_O_MINI;
-import static ee.carlrobert.llm.client.openai.completion.OpenAIChatCompletionModel.GPT_4_VISION_PREVIEW;
-
-import com.intellij.openapi.application.ApplicationManager;
 import ee.carlrobert.codegpt.CodeGPTBundle;
-import ee.carlrobert.codegpt.settings.service.codegpt.CodeGPTServiceSettings;
-import ee.carlrobert.codegpt.settings.service.openai.OpenAISettings;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public enum ServiceType {
@@ -63,35 +55,5 @@ public enum ServiceType {
       throw new RuntimeException("Provided client code '" + clientCode + "' is not supported");
     }
     return serviceType;
-  }
-
-  public boolean isImageActionSupported() {
-    return switch (this) {
-      case ANTHROPIC:
-      case OLLAMA:
-        yield true;
-      case CODEGPT:
-        var codegptModel = ApplicationManager.getApplication()
-            .getService(CodeGPTServiceSettings.class)
-            .getState()
-            .getChatCompletionSettings()
-            .getModel();
-        yield List.of(
-            "gpt-4o",
-            "gpt-4o-mini",
-            "claude-3-opus",
-            "claude-3.5-sonnet"
-        ).contains(codegptModel);
-      case OPENAI:
-        var openaiModel = ApplicationManager.getApplication().getService(OpenAISettings.class)
-            .getState()
-            .getModel();
-        yield List.of(
-            GPT_4_VISION_PREVIEW.getCode(),
-            GPT_4_O.getCode(),
-            GPT_4_O_MINI.getCode()).contains(openaiModel);
-      default:
-        yield false;
-    };
   }
 }
