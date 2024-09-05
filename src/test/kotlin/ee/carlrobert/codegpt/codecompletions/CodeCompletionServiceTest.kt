3ba9270430464f251836d464e2e2992779d0cdb3
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-09-05 15:11:12 +0300

    test: remove apply next word test

--- src/test/kotlin/ee/carlrobert/codegpt/codecompletions/CodeCompletionServiceTest.kt
+++ src/test/kotlin/ee/carlrobert/codegpt/codecompletions/CodeCompletionServiceTest.kt
@@ -3,7 +3,6 @@ package ee.carlrobert.codegpt.codecompletions
 import com.intellij.codeInsight.inline.completion.session.InlineCompletionSession.Companion.getOrNull
 import com.intellij.openapi.editor.VisualPosition
 import com.intellij.testFramework.PlatformTestUtil
-import ee.carlrobert.codegpt.CodeGPTKeys.REMAINING_EDITOR_COMPLETION
 import ee.carlrobert.codegpt.util.file.FileUtil
 import ee.carlrobert.llm.client.http.RequestEntity
 import ee.carlrobert.llm.client.http.exchange.StreamHttpExchange
@@ -85,71 +84,6 @@ class CodeCompletionServiceTest : IntegrationTest() {
         assertInlineSuggestion("Failed to display initial inline suggestion.") {
             "ublic void main" == it
         }
-    }
-
-    fun `test fetching next code completion`() {
-        useCodeGPTService()
-        myFixture.configureByText("CompletionTest.java", "")
-        expectCodeGPT(StreamHttpExchange { request: RequestEntity ->
-            assertThat(request.uri.path).isEqualTo("/v1/code/completions")
-            assertThat(request.method).isEqualTo("POST")
-            assertThat(request.body)
-                .extracting("model", "prefix", "suffix", "fileExtension")
-                .containsExactly("TEST_CODE_MODEL", "p", "", "java")
-            listOf(
-                jsonMapResponse("choices", jsonArray(jsonMap("text", "ublic static"))),
-                jsonMapResponse("choices", jsonArray(jsonMap("text", " void main(String"))),
-                jsonMapResponse("choices", jsonArray(jsonMap("text", "[] args) {\n"))),
-                jsonMapResponse("choices", jsonArray(jsonMap("text", "    System.out.print"))),
-                jsonMapResponse("choices", jsonArray(jsonMap("text", "ln(\"Hello, Worl"))),
-                jsonMapResponse("choices", jsonArray(jsonMap("text", "d!\");\n"))),
-                jsonMapResponse("choices", jsonArray(jsonMap("text", "}\n"))),
-                jsonMapResponse(
-                    "choices",
-                    jsonArray(jsonMap("text", "private static int getX() {\n"))
-                )
-            )
-        })
-        myFixture.type('p')
-        assertInlineSuggestion("Failed to display initial inline suggestion.") {
-            "ublic static void main(String[] args) {\n    System.out.println(\"Hello, World!\");" == it
-        }
-        assertThat(REMAINING_EDITOR_COMPLETION.get(myFixture.editor)).isEqualTo(
-            "ublic static void main(String[] args) {\n" +
-                    "    System.out.println(\"Hello, World!\");\n" +
-                    "}\n" +
-                    "private static int getX() {"
-        )
-        expectCodeGPT(StreamHttpExchange { request: RequestEntity ->
-            assertThat(request.uri.path).isEqualTo("/v1/code/completions")
-            assertThat(request.method).isEqualTo("POST")
-            assertThat(request.body)
-                .extracting("model", "prefix", "suffix", "fileExtension")
-                .containsExactly(
-                    "TEST_CODE_MODEL",
-                    "public static void main(String[] args) {\n" +
-                            "    System.out.println(\"Hello, World!\");\n" +
-                            "}\n" +
-                            "private static int getX() {",
-                    "",
-                    "java"
-                )
-            listOf(
-                jsonMapResponse("choices", jsonArray(jsonMap("text", "\n    retur"))),
-                jsonMapResponse("choices", jsonArray(jsonMap("text", "n 10;\n"))),
-                jsonMapResponse("choices", jsonArray(jsonMap("text", "}\n"))),
-            )
-        })
-
-        myFixture.type('\t')
-
-        PlatformTestUtil.waitWithEventsDispatching(
-            "Failed to retrieve next completion",
-            {
-                REMAINING_EDITOR_COMPLETION.get(myFixture.editor) == "\n}\nprivate static int getX() {"
-            },
-            10
-        )
     }
 
     private fun assertInlineSuggestion(errorMessage: String, onAssert: (String) -> Boolean) {
