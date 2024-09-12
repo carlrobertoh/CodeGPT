5d0d8f2b632f0e6cab64bee4f775564ea28e62fe
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-09-12 12:48:19 +0300

    fix: focus stealing when searching for suggestions

--- src/main/kotlin/ee/carlrobert/codegpt/ui/textarea/suggestion/SuggestionsPopupManager.kt
+++ src/main/kotlin/ee/carlrobert/codegpt/ui/textarea/suggestion/SuggestionsPopupManager.kt
@@ -63,13 +63,13 @@ class SuggestionsPopupManager(
     }
 
     fun selectNext() {
-        list.requestFocus()
         list.selectNext()
+        list.requestFocus()
     }
 
     fun selectPrevious() {
-        list.requestFocus()
         list.selectPrevious()
+        list.requestFocus()
     }
 
     fun updateSuggestions(searchText: String? = null) {
@@ -83,7 +83,6 @@ class SuggestionsPopupManager(
                 listModel.addAll(suggestions)
                 list.revalidate()
                 list.repaint()
-                selectNext()
             }
         }
     }
