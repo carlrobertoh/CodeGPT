1a159193a54c0a77173ec5d1b808ceeda8ea4289
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-09-23 14:57:28 +0300

    Revert "feat: add openai o1 models"
    This reverts commit 07fa79ce268a2096f0fa3a4a7472b5cbe544e961.

--- src/main/kotlin/ee/carlrobert/codegpt/settings/service/codegpt/CodeGPTAvailableModels.kt
+++ src/main/kotlin/ee/carlrobert/codegpt/settings/service/codegpt/CodeGPTAvailableModels.kt
@@ -14,8 +14,7 @@ object CodeGPTAvailableModels {
     fun getToolWindowModels(pricingPlan: PricingPlan?): List<CodeGPTModel> {
         return when (pricingPlan) {
             null, ANONYMOUS -> listOf(
-                CodeGPTModel("o1-preview", "o1-preview", Icons.OpenAI, INDIVIDUAL),
-                CodeGPTModel("o1-mini", "o1-mini", Icons.OpenAI, FREE),
+                CodeGPTModel("GPT-4o", "gpt-4o", Icons.OpenAI, INDIVIDUAL),
                 CodeGPTModel("Claude 3.5 Sonnet", "claude-3.5-sonnet", Icons.Anthropic, INDIVIDUAL),
                 CodeGPTModel("Llama 3.1 (405B)", "llama-3.1-405b", Icons.Meta, INDIVIDUAL),
                 CodeGPTModel("DeepSeek Coder V2", "deepseek-coder-v2", Icons.DeepSeek, INDIVIDUAL),
@@ -24,16 +23,15 @@ object CodeGPTAvailableModels {
             )
 
             FREE -> listOf(
-                CodeGPTModel("o1-preview", "o1-preview", Icons.OpenAI, INDIVIDUAL),
-                CodeGPTModel("o1-mini", "o1-mini", Icons.OpenAI, FREE),
+                CodeGPTModel("GPT-4o", "gpt-4o", Icons.OpenAI, INDIVIDUAL),
                 CodeGPTModel("Claude 3.5 Sonnet", "claude-3.5-sonnet", Icons.Anthropic, INDIVIDUAL),
+                CodeGPTModel("GPT-4o mini", "gpt-4o-mini", Icons.OpenAI, ANONYMOUS),
                 CodeGPTModel("Llama 3 (70B)", "llama-3-70b", Icons.Meta, FREE),
                 CodeGPTModel("Mixtral (8x22B)", "mixtral-8x22b", Icons.CodeGPTModel, FREE),
                 CodeGPTModel("Code Llama (70B)", "codellama:chat", Icons.Meta, FREE),
             )
 
             INDIVIDUAL -> listOf(
-                CodeGPTModel("o1-preview", "o1-preview", Icons.OpenAI, INDIVIDUAL),
                 CodeGPTModel("GPT-4o", "gpt-4o", Icons.OpenAI, INDIVIDUAL),
                 CodeGPTModel("Claude 3 Opus", "claude-3-opus", Icons.Anthropic, INDIVIDUAL),
                 CodeGPTModel("Claude 3.5 Sonnet", "claude-3.5-sonnet", Icons.Anthropic, INDIVIDUAL),
@@ -46,8 +44,7 @@ object CodeGPTAvailableModels {
 
     @JvmStatic
     val ALL_CHAT_MODELS: List<CodeGPTModel> = listOf(
-        CodeGPTModel("o1-preview", "o1-preview", Icons.OpenAI, INDIVIDUAL),
-        CodeGPTModel("o1-mini", "o1-mini", Icons.OpenAI, FREE),
+        CodeGPTModel("GPT-4o", "gpt-4o", Icons.OpenAI, INDIVIDUAL),
         CodeGPTModel("GPT-4o mini", "gpt-4o-mini", Icons.OpenAI, ANONYMOUS),
         CodeGPTModel("Claude 3 Opus", "claude-3-opus", Icons.Anthropic, INDIVIDUAL),
         CodeGPTModel("Claude 3.5 Sonnet", "claude-3.5-sonnet", Icons.Anthropic, INDIVIDUAL),
