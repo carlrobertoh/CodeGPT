536a3f23a4ddd66631d814bb6af7f5437adc3a21
Carl-Robert Linnupuu <carlrobertoh@gmail.com>
2024-11-04 12:06:43 +0000

    chore: oai compatibility for ollama chat completions

--- src/main/kotlin/ee/carlrobert/codegpt/completions/factory/OllamaRequestFactory.kt
+++ src/main/kotlin/ee/carlrobert/codegpt/completions/factory/OllamaRequestFactory.kt
@@ -3,36 +3,26 @@ package ee.carlrobert.codegpt.completions.factory
 import com.intellij.openapi.components.service
 import ee.carlrobert.codegpt.completions.BaseRequestFactory
 import ee.carlrobert.codegpt.completions.ChatCompletionParameters
-import ee.carlrobert.codegpt.completions.CompletionRequestUtil.FIX_COMPILE_ERRORS_SYSTEM_PROMPT
-import ee.carlrobert.codegpt.completions.ConversationType
+import ee.carlrobert.codegpt.completions.factory.OpenAIRequestFactory.Companion.buildOpenAIMessages
 import ee.carlrobert.codegpt.settings.configuration.ConfigurationSettings
-import ee.carlrobert.codegpt.settings.persona.PersonaSettings
 import ee.carlrobert.codegpt.settings.service.ollama.OllamaSettings
-import ee.carlrobert.llm.client.ollama.completion.request.OllamaChatCompletionMessage
-import ee.carlrobert.llm.client.ollama.completion.request.OllamaChatCompletionRequest
-import ee.carlrobert.llm.client.ollama.completion.request.OllamaParameters
-import java.io.IOException
-import java.nio.file.Files
-import java.nio.file.Path
-import java.util.*
+import ee.carlrobert.llm.client.openai.completion.request.OpenAIChatCompletionRequest
+import ee.carlrobert.llm.completion.CompletionRequest
 
 class OllamaRequestFactory : BaseRequestFactory() {
 
-    override fun createChatRequest(params: ChatCompletionParameters): OllamaChatCompletionRequest {
+    override fun createChatRequest(params: ChatCompletionParameters): OpenAIChatCompletionRequest {
+        val model = service<OllamaSettings>().state.model
         val configuration = service<ConfigurationSettings>().state
-        val settings = service<OllamaSettings>().state
-        return OllamaChatCompletionRequest.Builder(
-            settings.model,
-            buildOllamaMessages(params)
-        )
-            .setStream(true)
-            .setOptions(
-                OllamaParameters.Builder()
-                    .numPredict(configuration.maxTokens)
-                    .temperature(configuration.temperature.toDouble())
-                    .build()
+        val requestBuilder: OpenAIChatCompletionRequest.Builder =
+            OpenAIChatCompletionRequest.Builder(
+                buildOpenAIMessages(model, params, params.referencedFiles)
             )
-            .build()
+                .setModel(model)
+                .setMaxTokens(configuration.maxTokens)
+                .setStream(true)
+                .setTemperature(configuration.temperature.toDouble())
+        return requestBuilder.build()
     }
 
     override fun createBasicCompletionRequest(
@@ -40,71 +30,13 @@ class OllamaRequestFactory : BaseRequestFactory() {
         userPrompt: String,
         maxTokens: Int,
         stream: Boolean
-    ): OllamaChatCompletionRequest {
-        return OllamaChatCompletionRequest.Builder(
-            service<OllamaSettings>().state.model,
-            listOf(
-                OllamaChatCompletionMessage("system", systemPrompt, null),
-                OllamaChatCompletionMessage("user", userPrompt, null)
-            )
+    ): CompletionRequest {
+        val model = service<OllamaSettings>().state.model
+        return OpenAIRequestFactory.createBasicCompletionRequest(
+            systemPrompt,
+            userPrompt,
+            model = model,
+            isStream = stream
         )
-            .setStream(stream)
-            .build()
-    }
-
-    private fun buildOllamaMessages(params: ChatCompletionParameters): List<OllamaChatCompletionMessage> {
-        val message = params.message
-        val messages = mutableListOf<OllamaChatCompletionMessage>()
-
-        when (params.conversationType) {
-            ConversationType.DEFAULT -> messages.add(
-                OllamaChatCompletionMessage("system", PersonaSettings.getSystemPrompt(), null)
-            )
-
-            ConversationType.FIX_COMPILE_ERRORS -> messages.add(
-                OllamaChatCompletionMessage("system", FIX_COMPILE_ERRORS_SYSTEM_PROMPT, null)
-            )
-
-            else -> {}
-        }
-
-        for (prevMessage in params.conversation.messages) {
-            if (params.retry && prevMessage.id == message.id) break
-
-            prevMessage.imageFilePath?.takeIf { it.isNotEmpty() }?.let { imagePath ->
-                try {
-                    val imageBytes = Files.readAllBytes(Path.of(imagePath))
-                    val imageBase64 = Base64.getEncoder().encodeToString(imageBytes)
-                    messages.add(
-                        OllamaChatCompletionMessage(
-                            "user",
-                            prevMessage.prompt,
-                            listOf(imageBase64)
-                        )
-                    )
-                } catch (e: IOException) {
-                    throw RuntimeException(e)
-                }
-            } ?: run {
-                messages.add(
-                    OllamaChatCompletionMessage(
-                        "user",
-                        getPromptWithFilesContext(params),
-                        null
-                    )
-                )
-            }
-
-            messages.add(OllamaChatCompletionMessage("assistant", prevMessage.response, null))
-        }
-
-        if (params.imageMediaType != null && params.imageData != null) {
-            val imageBase64 = Base64.getEncoder().encodeToString(params.imageData)
-            messages.add(OllamaChatCompletionMessage("user", message.prompt, listOf(imageBase64)))
-        } else {
-            messages.add(OllamaChatCompletionMessage("user", message.prompt, null))
-        }
-
-        return messages
     }
 }
