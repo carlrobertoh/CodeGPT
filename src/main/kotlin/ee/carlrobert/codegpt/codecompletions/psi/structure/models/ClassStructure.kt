890cf5d6ccb65301bdc5388da89d44ea57b0c4b4
a.iudin <a.iudin@vk.team>
2025-01-20 17:48:41 +0300

    Add Kotlin PSI structure analysis and serialization components

---
+++ src/main/kotlin/ee/carlrobert/codegpt/codecompletions/psi/structure/models/ClassStructure.kt
@@ -0,0 +1,17 @@
+package ee.carlrobert.codegpt.codecompletions.psi.structure.models
+
+data class ClassStructure(
+    val name: ClassName,
+    val simpleName: ClassName,
+    val classType: ClassType,
+    val modifierList: List<String>,
+    val packageName: String,
+    val repositoryName: String,
+    val lang: ClassLanguage = ClassLanguage.KOTLIN,
+    val constructors: MutableList<ConstructorStructure> = mutableListOf(),
+    val fields: MutableList<FieldStructure> = mutableListOf(),
+    val methods: MutableList<MethodStructure> = mutableListOf(),
+    val supertypes: MutableList<ClassName> = mutableListOf(),
+    val enumEntries: MutableList<EnumEntryName> = mutableListOf(),
+    val classes: MutableList<ClassStructure> = mutableListOf()
+)
